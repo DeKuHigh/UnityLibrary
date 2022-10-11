e4e63e795ce93fea06cd3aef85aa41a74a1f91ed
Akhil Sharma <akhilsharma.off@gmail.com>
2022-10-11 19:05:45 +0530

    new library

---
+++ AkhilUnity/AkhilUnity/Traceable.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AkhilUnity
+{
+    /// <summary>
+    /// Tried to implement to traceable class for Unity Engine
+    /// </summary>
+    public class Traceable:MonoBehaviour
+    {
+        public Image image;
+
+     
+    }
+}
