fa5991504d55b515b6bd4a6854a0b235374f9ada
DeKuHigh <115560513+DeKuHigh@users.noreply.github.com>
2022-10-11 19:04:23 +0530

    basic intro of the library

---
+++ TestingDLL/TestingDLL/MathService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestingDLL
+{
+    public static class MathService
+    {
+
+        public static int Divide(int x, int y)
+        {
+            if(y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return x / y;
+        }
+        
+    }
+}
