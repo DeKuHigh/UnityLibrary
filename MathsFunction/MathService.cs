bc118f884ba222c76bf11fabc44409f1dfe15122
Akhil Sharma <akhilsharma.off@gmail.com>
2022-10-18 20:00:04 +0530

    Maths class implemented for the exception handling

---
+++ MathsFunction/MathService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestingDLL
+{
+    // The basic example for the DivideByZeroException() 
+    public static class MathService
+    {
+
+        public static int Divide(int x, int y)
+        {
+            if(y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return x / y;
+        }
+        
+    }
+}
